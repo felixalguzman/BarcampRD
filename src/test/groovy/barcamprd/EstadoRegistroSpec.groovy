8ae0ddcc7c0751c4e602c63ca45cbbed1ec56902
dewyn <dewyn.liriano@gmail.com>
2018-10-26 17:20:50 -0400

    Deshabilitando admin

---
+++ src/test/groovy/barcamprd/EstadoRegistroSpec.groovy
@@ -0,0 +1,18 @@
+package barcamprd
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class EstadoRegistroSpec extends Specification implements DomainUnitTest<EstadoRegistro> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
