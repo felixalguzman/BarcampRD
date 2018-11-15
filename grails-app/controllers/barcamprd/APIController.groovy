860a19dcb6e27d77576e8f0584df0bc2b02f0104
dewyn <dewyn.liriano@gmail.com>
2018-11-15 00:49:13 -0400

    Creando API para consumo móvil.

---
+++ grails-app/controllers/barcamprd/APIController.groovy
@@ -0,0 +1,23 @@
+package barcamprd
+
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['permitAll'])
+class APIController {
+
+    def index() { }
+
+    def registros(){
+        render Registro.list() as JSON
+    }
+
+    def confirmar(){
+        long id = params.id
+        def registro = Registro.findById(id)
+        println(registro.nombre)
+        /*registro.estado = EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_CONFIRMADO)
+        registro.save(flush: true, failOnError: true)*/
+        return registro.estado.texto
+    }
+}
