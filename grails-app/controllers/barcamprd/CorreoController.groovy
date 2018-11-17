66f2c4196c85ca7d2150017e058bd381cf99d067
Carlos Camacho <ca.camacho@ce.pucmm.edu.do>
2018-11-16 20:33:16 -0400

    Incluyendo restapi para enviar correo de la vista.

--- grails-app/controllers/barcamprd/CorreoController.groovy
+++ grails-app/controllers/barcamprd/CorreoController.groovy
@@ -1,5 +1,7 @@
 package barcamprd
 
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.Unirest
 import com.sparkpost.Client
 import com.sparkpost.exception.SparkPostException
 import org.springframework.security.access.annotation.Secured
@@ -7,9 +9,13 @@ import org.springframework.security.access.annotation.Secured
 @Secured(["permitAll"])
 class CorreoController {
 
-    def index() throws SparkPostException {
-        def lista = Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))
-        [participantes : Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))]
+    def groovyPageRenderer;
+
+    def proceso() throws SparkPostException {
+
+        //String salida = g.render(view: "index", model: model)
+        HttpResponse<String> salida =  Unirest.get("http://localhost:8080/correo/").asString()
+        println "la salida: "+salida.body
         String API_KEY = "7cc9b263021914c819d287b6ffc3bda8e90fd9d3";
         Client client = new Client(API_KEY);
         client.sendMessage(
@@ -17,10 +23,12 @@ class CorreoController {
                     "dewyn.liriano@gmail.com",
                     "Información importante Barcamp 2018",
                     "",
-                    "")
+                    salida.body)
+       render "Enviando....."
     }
 
-    def enviar(){
-
+    def index(){
+        def lista = Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))
+        [participantes : Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))]
     }
 }
