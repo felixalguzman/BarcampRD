ea99327e6754153a2c61be940dd8b7879e9459d6
Carlos <carlosperezurena@gmail.com>
2018-11-16 20:12:53 -0400

    Arreglando controllerr de correo

--- grails-app/controllers/barcamprd/CorreoController.groovy
+++ grails-app/controllers/barcamprd/CorreoController.groovy
@@ -10,32 +10,17 @@ class CorreoController {
     def index() throws SparkPostException {
         def lista = Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))
         [participantes : Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))]
-        /*String API_KEY = "7cc9b263021914c819d287b6ffc3bda8e90fd9d3";
+        String API_KEY = "7cc9b263021914c819d287b6ffc3bda8e90fd9d3";
         Client client = new Client(API_KEY);
         client.sendMessage(
                     "logistica@barcamp.org.do",
                     "dewyn.liriano@gmail.com",
                     "Información importante Barcamp 2018",
-                    "Hola",
-                    "");
-        render "index"*/
+                    "",
+                    "")
     }
 
     def enviar(){
 
-        /*def plantillaCorreo = g.render(template: "index", mo)*/
-//        String API_KEY = "7cc9b263021914c819d287b6ffc3bda8e90fd9d3"
-//        Client client = new Client(API_KEY)
-//
-//        try {
-//            client.sendMessage(
-//                    "logistica@barcamp.org.do",
-//                    "carlosperezurena@gmail.com",
-//                    "Información importante Barcamp 2018",
-//                    "",
-//                    g.render(template: "index"))
-//        }catch(SparkPostException e){
-//
-//        }
     }
 }
--- grails-app/views/correo/index.gsp
+++ grails-app/views/correo/index.gsp
@@ -6,12 +6,12 @@
 <body>
     <div id="content" role="main">
         <section class="row colset-2-its">
-            <h5>Buenas tardes, ${participantes.get(0).nombre}</h5>
-            %{--<br>--}%
+            <h5>Buenas noches, ${participantes.get(0).nombre}</h5>
+
             <p>Este Sábado 17 de noviembre a las 8:00 AM empezará el recibimiento a los participantes del evento</p>
-            %{--<br>--}%
-            <p>Será en los salones octogonales del edificio Padre Arroyo(Segunda planta del edificio, cerca del teatro universitario).</p>
-            %{--<br>--}%
+
+            <p>Será en los salones octogonales del edificio Padre Arroyo (Segunda planta del edificio, cerca del teatro universitario).</p>
+
             <p>Adjunto a este correo encontrará el código de conducta del evento. Esperamos lo lea y acepte todas las condiciones indicadas en el mismo.</p>
             <br>
             <h4>Las charlas en las que se ha reservado un asiento para usted son las siguientes:</h4>
