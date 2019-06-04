a4b64ae8ea5dcfe19504d5afeda73d5db124c65e
Felix A. Guzman <felixalejandro@outlook.com>
2019-06-03 21:38:26 -0400

    Modificando api

--- grails-app/controllers/barcamprd/APIController.groovy
+++ grails-app/controllers/barcamprd/APIController.groovy
@@ -11,10 +11,10 @@ class APIController {
     }
 
     /**
-     * 
+     *
      * @return
      */
-    def registros(){
+    def registros() {
         def lista = Registro.findAllByEstado(EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_APROBADO))
         def listMap = []
         lista.each {
@@ -29,47 +29,61 @@ class APIController {
     }
 
     /**
-     * 
+     *
      * @param id
      * @return
      */
-    def consultarRegistro(long id){
+    def consultarRegistro(long id) {
         println "......"
         def registro = Registro.findById(id)
 
         def map = [:]
 
-        if (registro){
+        if (registro) {
             map['id'] = registro.id
             map['nombre'] = registro.nombre
             map['cedula'] = registro.cedula
             map['size'] = registro.sizeCamiseta
             render map as JSON
-        } else{
+        } else {
             response.status = 400
             render('Registro no existe')
         }
     }
 
     /**
-     * 
+     *
      * @param id
      * @return
      */
-    def confirmar(long id){
+    def confirmar(long id) {
         def registro = Registro.findById(id)
-        if(registro) {
+        if (registro) {
             def antes = registro.estado.texto
             registro.estado = EstadoRegistro.findByNumero(EstadoRegistro.ESTADO_CONFIRMADO)
             registro.save(flush: true, failOnError: true)
             render "Cambiando de " + antes + " a " + registro.estado.texto
-        } else{
+        } else {
             response.status = 400
             render('Registro no existe')
         }
     }
 
-    def charlas(){
+    def registroUsuario(String email) {
+        def registro = Registro.findByCorreo(email)
+        if (registro) {
+            def map = [:]
+            map['nombre'] = registro.nombre
+            map['correo'] = registro.correo
+            map['charlas'] = registro.listaCharlas
+            render map as JSON
+        } else {
+            response.status = 404
+            render "Registro no encontrado"
+        }
+    }
+
+    def charlas() {
         def charlas = Charla.findAll()
         render charlas as JSON
     }
